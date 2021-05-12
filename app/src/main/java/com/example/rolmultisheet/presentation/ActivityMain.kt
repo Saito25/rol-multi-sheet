105fab5d3c7d86001513c0333ffbdeee83280a8a
manuelcodigobase <manuel.perez@codigobase.es>
2021-05-12 18:35:15 +0200

    Created menu

--- app/src/main/java/com/example/rolmultisheet/presentation/ActivityMain.kt
+++ app/src/main/java/com/example/rolmultisheet/presentation/ActivityMain.kt
@@ -1,9 +1,11 @@
-package com.example.rolmultisheet
+package com.example.rolmultisheet.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rolmultisheet.R
 
-class MainActivity : AppCompatActivity() {
+class ActivityMain : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/rolmultisheet/presentation/fragments/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.rolmultisheet.presentation.fragments.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rolmultisheet.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
