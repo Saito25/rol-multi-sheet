abc8dbb30b6fff672eafc4ba54253d2abc03e283
manuelcodigobase <manuel.perez@codigobase.es>
2021-05-14 21:08:37 +0200

    Created app database

--- app/src/main/java/com/example/rolmultisheet/data/dao/AppDao.kt
+++ app/src/main/java/com/example/rolmultisheet/data/dao/AppDao.kt
@@ -1,11 +1,15 @@
 package com.example.rolmultisheet.data.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.rolmultisheet.domain.model.Character
 
 interface AppDao {
 
     // Query
+    @Query("SELECT * FROM character")
+    fun queryAllCharacters(): LiveData<List<Character>>
 
     // Insert
     @Insert()
