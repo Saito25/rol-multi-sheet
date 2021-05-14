466b95e131cd7a4df3fbb2863a7d0de332976e4a
manuelcodigobase <manuel.perez@codigobase.es>
2021-05-14 20:59:37 +0200

    Created character model

---
+++ app/src/main/java/com/example/rolmultisheet/domain/model/Character.kt
@@ -0,0 +1,14 @@
+package com.example.rolmultisheet.domain.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "character")
+data class Character(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "character_id")
+    val characterId: Long = 0,
+    @ColumnInfo(name = "character_id")
+    val characterName: String,
+)
