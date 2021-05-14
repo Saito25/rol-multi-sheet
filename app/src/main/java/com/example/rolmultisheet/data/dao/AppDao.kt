3bf76e047b976a059b43aad7b4f5c6c7e1d8fa93
manuelcodigobase <manuel.perez@codigobase.es>
2021-05-14 21:14:09 +0200

    Fixed: Dao interface must be mark like @Dao

--- app/src/main/java/com/example/rolmultisheet/data/dao/AppDao.kt
+++ app/src/main/java/com/example/rolmultisheet/data/dao/AppDao.kt
@@ -1,10 +1,12 @@
 package com.example.rolmultisheet.data.dao
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.rolmultisheet.domain.model.Character
 
+@Dao
 interface AppDao {
 
     // Query
