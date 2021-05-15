7bbcfa86789ef37a1269e1f3158a45da651a2219
manuelcodigobase <manuel.perez@codigobase.es>
2021-05-15 20:55:11 +0200

    Created race edition direction and action from game tab

---
+++ app/src/main/java/com/example/rolmultisheet/presentation/fragment/race/edition/RaceEditionFragment.kt
@@ -0,0 +1,6 @@
+package com.example.rolmultisheet.presentation.fragment.race.edition
+
+import androidx.fragment.app.Fragment
+import com.example.rolmultisheet.R
+
+class RaceEditionFragment : Fragment(R.layout.race_edition_fragment)
---
+++ app/src/main/java/com/example/rolmultisheet/presentation/fragment/race/edition/RaceEditionViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.rolmultisheet.presentation.fragment.race.edition
+
+import androidx.lifecycle.ViewModel
+
+class RaceEditionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
