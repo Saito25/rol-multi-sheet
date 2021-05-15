1358fb259fd89c393173c04e0a8eeab471cc0588
manuelcodigobase <manuel.perez@codigobase.es>
2021-05-15 20:10:47 +0200

    Added job (class) entity

---
+++ app/src/main/java/com/example/rolmultisheet/domain/model/Job.kt
@@ -0,0 +1,21 @@
+package com.example.rolmultisheet.domain.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// To avoid errors with the name, this class is called Job, but represent a class instead.
+@Entity(tableName = "job")
+data class Job(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "job_id", index = true)
+    val jobId: Long = 0,
+    @ColumnInfo(name = "job_name")
+    val jobName: String,
+    @ColumnInfo(name = "job_hit")
+    val jobHit: String,
+    @ColumnInfo(name = "job_feature")
+    val jobFeature: String,
+    @ColumnInfo(name = "job_save_throw")
+    val jobSaveThrow: String,
+)
