d21078da8769fe01d001db4a0603d0031b229f57
manuelcodigobase <manuel.perez@codigobase.es>
2021-06-01 18:56:15 +0200

    Created initial structure for CharacterSpellListFragment

--- app/src/main/java/com/example/rolmultisheet/presentation/fragment/character/spell/CharacterSpellListFragment.kt
+++ app/src/main/java/com/example/rolmultisheet/presentation/fragment/character/spell/CharacterSpellListFragment.kt
@@ -1,6 +1,58 @@
 package com.example.rolmultisheet.presentation.fragment.character.spell
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rolmultisheet.R
+import com.example.rolmultisheet.data.database.AppDatabase
+import com.example.rolmultisheet.data.repository.RoomRepository
+import com.example.rolmultisheet.databinding.CommonListFragmentBinding
+import com.example.rolmultisheet.presentation.fragment.spell.main.SpellMainListAdapter
+import com.example.rolmultisheet.presentation.util.fragment.ArgumentsOwner
+import com.example.rolmultisheet.presentation.util.fragment.viewBinding
+import com.example.rolmultisheet.presentation.util.tab.PageFragment
 
-class CharacterSpellListFragment : Fragment(R.layout.spell_main_item_fragment)
+class CharacterSpellListFragment : PageFragment(R.layout.common_list_fragment) {
+
+    private val binding: CommonListFragmentBinding by viewBinding {
+        CommonListFragmentBinding.bind(it)
+    }
+
+    private val viewModel: CharacterSpellListViewModel by viewModels {
+        CharacterSpellListViewModelFactory(
+            RoomRepository(AppDatabase.getInstance(requireContext()).appDao),
+            (requireParentFragment() as ArgumentsOwner).characterId
+        )
+    }
+
+    private val listAdapter by lazy {
+        SpellMainListAdapter()
+    }
+
+    override fun onFabClick() {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        binding.listCommonRoot.run {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+            itemAnimator = DefaultItemAnimator()
+            adapter = listAdapter
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rolmultisheet/presentation/fragment/character/spell/CharacterSpellListViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.rolmultisheet.presentation.fragment.character.spell
+
+import androidx.lifecycle.ViewModel
+import com.example.rolmultisheet.domain.repository.AppRepository
+
+class CharacterSpellListViewModel(appRepository: AppRepository, characterId: Long) : ViewModel()
